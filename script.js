d06092c5c6b072951963fd1d046c832b7233b82f
Atul-raj07 <atulrajgupta12j@gmail.com>
2023-12-17 23:09:09 +0530

    sight banking website clone half part made

---
+++ script.js
@@ -0,0 +1,32 @@
+let card1 = document.querySelector(".card1");
+let card2 = document.querySelector(".card2");
+let page1 = document.querySelector("#page1");
+
+page1.addEventListener("mousemove",function(dets){
+    
+    let mousex = dets.clientX;
+    let ttlwidth = page1.offsetWidth;
+    let rotationangle = (mousex / ttlwidth - 0.5)*100;
+
+    card1.style.transform = `rotateX(${rotationangle}deg)`;
+    card2.style.transform = `rotateX(${rotationangle}deg)`;
+})
+let imagesecond = document.querySelector(".images:nth-child(2)");
+let img = document.querySelector(".images:nth-child(2) img:nth-child(2)")
+let icon = document.querySelector(".icon")
+// let iconafter = document.querySelector(icon, `::after`)
+imagesecond.addEventListener("mouseenter",function(){
+    img.style.transform = "scale(0.8)";
+    icon.style.position = 'absolute';
+    icon.style.top = "0%";
+    icon.style.right = "0%";
+    // iconafter.style.display = "inline"
+
+})
+imagesecond.addEventListener("mouseleave",function(){
+    img.style.transform = "scale(1)";
+    icon.style.position = 'absolute';
+    icon.style.top = "5%";
+    icon.style.right = "5%";
+    // icon.style.display = "inline";
+})
